e5756761e6a0915beb4508a84485d7288c31f0b6
winvis <endohizumi0114@gmail.com>
2016-09-20 01:14:50 +0900

    Add Action when the other player has entered.

--- GameRoom.js
+++ GameRoom.js
@@ -4,6 +4,19 @@ $(document).ready(function () {
         console.log(event);
         var jdata = $.parseJSON(event.data);
         console.log(jdata.ID + " " + jdata.Name + " " + jdata.Event + " send time " + jdata.time + "<br>");
+        if (jdata.Event == "Enter") {
+            AddMember(jdata);
+        }
     };
 
+    function AddMember(jdata) {
+        console.log("Enter " + jdata.Name);
+        $(".PlayersView").append('<div class="Player shadow" id="' + jdata.Name + '"><img src="icon.png"><span class="name">' + jdata.Name + '</span></div>');
+    }
 });
+//
+// <div class="Player shadow" id="hoge">
+//            <!--DisplayTest User -->
+//            <img src="icon.png">
+//            <span class="name">Hoge</span>
+//        </div>
