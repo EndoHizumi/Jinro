d4e09f7adc9f47c3e72ed35138c717a911733fe9
winvis <winvissoftware@gmail.com>
2016-09-22 22:34:56 +0900

    Layout Fixed

--- Common.php
+++ Common.php
@@ -1,7 +1,8 @@
 <?php
 try {
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=winvis;charset=utf8','root','',
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=winvis;charset=utf8','root','php1850',
 array(PDO::ATTR_EMULATE_PREPARES => false));
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
  echo('データベース接続失敗。'.$e->getMessage());
  return;
