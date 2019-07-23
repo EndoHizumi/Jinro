47ed226c2746165a66c115f75e83100c0ebeecac
endohizumi <endohizumi0114@gmail.com>
2019-07-23 19:09:56 +0900

    update add create table statement

--- mysql_init/init.sql
+++ mysql_init/init.sql
@@ -1,1 +1,3 @@
-create database winvis;
+create database jinro;
+create table jinro.member (id int,name varchar(255),ready int,expel int,guard int,event varchar(255),job varchar(255),vote int);
+create table jinro.actors (peoples int,Villager int,wolf int,fortune int,hunter int,cracker int,mystic int);
