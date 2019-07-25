174c4489ad2e716811fa48266fff6ac1f4ba8627
endohizumi <endohizumi0114@gmail.com>
2019-07-25 17:32:10 +0900

    update Add query for initial data input

--- mysql_init/init.sql
+++ mysql_init/init.sql
@@ -1,3 +1,5 @@
 create database jinro;
-create table jinro.member (id int,name varchar(255),ready int,expel int,guard int,event varchar(255),job varchar(255),vote int);
-create table jinro.actors (peoples int,Villager int,wolf int,fortune int,hunter int,cracker int,mystic int);
+use jinro;
+create table members (id int,name varchar(255),ready int,expel int,guard int,event varchar(255),job varchar(255),vote int);
+create table actors (peoples int,Villager int,wolf int,fortune int,hunter int,cracker int,mystic int);
+INSERT INTO members VALUES (0,"room",0,0,0,'',"koke",0);
