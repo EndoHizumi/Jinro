d157b072d50df3da9dafc768a58dde075d317b54
endohizumi <endohizumi0114@gmail.com>
2019-07-25 17:32:53 +0900

    update Add TRIGGER to deliver game status.

--- mysql_init/init.sql
+++ mysql_init/init.sql
@@ -3,3 +3,7 @@ use jinro;
 create table members (id int,name varchar(255),ready int,expel int,guard int,event varchar(255),job varchar(255),vote int);
 create table actors (peoples int,Villager int,wolf int,fortune int,hunter int,cracker int,mystic int);
 INSERT INTO members VALUES (0,"room",0,0,0,'',"koke",0);
+create table activity_logs (ID int NOT NULL AUTO_INCREMENT,Name varchar(255),Event varchar(255),time datetime,primary key (id));
+CREATE TRIGGER updateMember AFTER UPDATE ON members FOR EACH ROW insert into activity_logs(name,event,time) value(new.name,new.event,Now());
+CREATE TRIGGER insertMember AFTER INSERT ON members FOR EACH ROW insert into activity_logs(name,event,time) value(new.name,new.event,Now());
+CREATE TRIGGER deleteMember BEFORE DELETE ON members FOR EACH ROW insert into activity_logs(name,event,time) value(old.name,old.event,Now());
